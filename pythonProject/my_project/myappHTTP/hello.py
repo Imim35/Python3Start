055a347d45a74260687b68edfd163e3b0267c75c
Imim35 <genia_imim@mail.ru>
2024-08-04 17:15:37 +0400

    init fix

--- pythonProject/my_project/myappHTTP/hello.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-
-
-def hello(request, number):
-    text = "<h1>welcome to my app number %s!</h1>" % number
-    return HttpResponse(text)
--- pythonProject/my_project/products/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-
-# Create your views here.
-
-def index(request):
-    # other view code here
-    return render(request, 'products.html', {})
